b6e461a1ed81875bb558fc7d76953d622eaaf3ef
“Loren <couselm@gmail.com>
2024-08-03 11:51:23 +0700

    Add city select and user location

--- components/locations/LocationCard.tsx
+++ components/locations/LocationCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
-import { Card, Avatar, Text, Icon } from 'react-native-paper';
+import { Card, Avatar, Text, Icon, IconButton } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import { GOOGLE_PLACES_API_KEY } from '@env';
 
@@ -10,8 +10,8 @@ const LocationCard = ({ item }) => {
   const iconName = item.types?.includes('cafe')
     ? 'coffee'
     : item.types?.includes('library')
-    ? 'library'
-    : 'domain';
+      ? 'library'
+      : 'domain';
   const placeName = item.name || item.tags?.name || '';
   const amenityName = item.types?.[0]?.replace(/_/g, ' ') || '';
 
@@ -22,42 +22,33 @@ const LocationCard = ({ item }) => {
       }
       style={styles.card}
     >
-      <View style={styles.container}>
-        {item.photos?.[0] ? (
-          <Image
-            source={{
-              uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${item.photos[0].photo_reference}&key=${GOOGLE_PLACES_API_KEY}`,
-            }}
-            style={styles.image}
-          />
-        ) : (
-          <Avatar.Icon size={75} icon={iconName} backgroundColor='green' />
-        )}
+      {item.photos?.[0] && (
+        <Card.Cover
+          source={{
+            uri: `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${item.photos[0].photo_reference}&key=${GOOGLE_PLACES_API_KEY}`,
+          }}
+        />
+      )}
 
-        <View style={styles.content}>
-          <Text variant='titleMedium'>{placeName}</Text>
-          {item.rating && (
-            <View style={styles.ratingContainer}>
-              <Icon name='star' source='star' size={16} />
-              <Text style={styles.ratingText}>
-                {item.rating} ({item.user_ratings_total})
-              </Text>
-            </View>
-          )}
-        </View>
-
-        <View style={styles.rightContent}>
-          <View style={styles.amenityContainer}>
-            <Icon name={iconName} source={iconName} size={20} />
-            <Text style={styles.amenityText}>{amenityName}</Text>
+      <Card.Title
+        title={placeName}
+        subtitle={
+          item.opening_hours && item.opening_hours.open_now ? 'Open' : 'Closed'
+        }
+        left={(props) => <Avatar.Icon {...props} icon={iconName} />}
+        right={(props) => (
+          <View>
+            {item.rating && (
+              <View style={styles.row}>
+                <Icon name='star' source='star' size={20} color='#FFD700' />
+                <Text>
+                  {item.rating} ({item.user_ratings_total})
+                </Text>
+              </View>
+            )}
           </View>
-          {item.opening_hours?.open_now !== undefined && (
-            <Text style={styles.openStatusText}>
-              {item.opening_hours.open_now ? 'Open' : 'Closed'}
-            </Text>
-          )}
-        </View>
-      </View>
+        )}
+      />
     </Card>
   );
 };
@@ -65,42 +56,9 @@ const LocationCard = ({ item }) => {
 const styles = StyleSheet.create({
   card: {
     marginBottom: 15,
-    padding: 10,
   },
-  container: {
+  row: {
     flexDirection: 'row',
-    alignItems: 'center',
-  },
-  image: {
-    width: 75,
-    height: 75,
-    borderRadius: 5,
-  },
-  content: {
-    flex: 1,
-    marginLeft: 10,
-  },
-  rightContent: {
-    alignItems: 'flex-end',
-  },
-  ratingContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 5,
-  },
-  ratingText: {
-    marginLeft: 5,
-  },
-  amenityContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  amenityText: {
-    textTransform: 'capitalize',
-    marginLeft: 5,
-  },
-  openStatusText: {
-    marginTop: 5,
   },
 });
 
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,122 @@
-import React from 'react';
-import { Button, Text, SegmentedButtons } from 'react-native-paper';
-import { View, StyleSheet, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import { Button, Text, Searchbar, List } from 'react-native-paper';
+import * as Location from 'expo-location';
+import axios from 'axios';
+import { GOOGLE_PLACES_API_KEY } from '@env';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function HomeScreen({ navigation }) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [region, setRegion] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const getUserLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    const { latitude, longitude } = location.coords;
+
+    setRegion({
+      latitude,
+      longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${GOOGLE_PLACES_API_KEY}`,
+    );
+
+    const city = response.data.results.find((result) =>
+      result.types.includes('locality'),
+    )?.address_components?.[0]?.long_name;
+
+    if (city) {
+      setSearchQuery(city);
+    } else {
+      alert('City not found');
+    }
+  };
+
+  const fetchSuggestions = async (query) => {
+    setLoading(true);
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${query}&types=(cities)&key=${GOOGLE_PLACES_API_KEY}`,
+    );
+    setSuggestions(response.data.predictions);
+    setLoading(false);
+  };
+
+  const searchUserLocation = () => {
+    fetchSuggestions(searchQuery);
+  };
+
+  const handleSuggestionPress = async (description, placeId) => {
+    setSearchQuery(description);
+    setSuggestions([]);
+
+    // Get the coordinates for the selected place
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${GOOGLE_PLACES_API_KEY}`,
+    );
+
+    const { lat, lng } = response.data.result.geometry.location;
+    setRegion({
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Find Today's Workspace</Text>
+      <Searchbar
+        style={styles.searchBar}
+        placeholder='Search'
+        onChangeText={(query) => {
+          setSearchQuery(query);
+          fetchSuggestions(query);
+        }}
+        value={searchQuery}
+        icon='magnify'
+        onIconPress={searchUserLocation}
+        traileringIcon='near-me'
+        onTraileringIconPress={getUserLocation}
+      />
+      {suggestions.length > 0 && (
+        <FlatList
+          data={suggestions}
+          keyExtractor={(item) => item.place_id}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              onPress={() =>
+                handleSuggestionPress(item.description, item.place_id)
+              }
+            >
+              <List.Item title={item.description} />
+            </TouchableOpacity>
+          )}
+        />
+      )}
       <Button
         icon='magnify'
         mode='elevated'
@@ -15,6 +126,14 @@ export default function HomeScreen({ navigation }) {
       >
         Locations
       </Button>
+      <MapView style={styles.map} region={region}>
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+          }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -22,11 +141,21 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    paddingTop: 50,
   },
   welcomeText: {
     fontSize: 20,
     marginBottom: 20,
+  },
+  searchBar: {
+    width: '90%',
+    marginBottom: 20,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: 300,
+    marginTop: 20,
   },
 });
